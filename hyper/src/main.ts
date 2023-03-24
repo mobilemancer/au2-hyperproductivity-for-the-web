ef87611d470e4c74b4282bad743860dda494974d
Andreas Wänqvist <andrkarl@gmail.com>
2023-03-24 23:14:15 +0100

    feat: games

--- hyper/src/main.ts
+++ hyper/src/main.ts
@@ -4,10 +4,12 @@ import { MyApp } from './my-app';
 
 import * as personsModule from "./pages/4star-wars-persons/persons-registry";
 import * as filmsModule from "./pages/5star-wars-films/films-registry";
+import * as gamesModule from "./pages/6star-wars-games/games-registry";
 
 Aurelia
   .register(personsModule)
   .register(filmsModule)
+  .register(gamesModule)
   .register(RouterConfiguration.customize({ useUrlFragmentHash: false }))
   .app(MyApp)
   .start();
--- hyper/src/pages/6star-wars-games/star-wars-games.ts
+++ hyper/src/pages/6star-wars-games/star-wars-games.ts
@@ -1,1 +1,13 @@
-export class Games
+export class StarWarsGames {
+    private token = "ring";
+    public clickHandler(event) {
+        console.log(event);
+
+        event.target.classList.add(this.token);
+        if (this.token === "ring") {
+            this.token = "cross";
+        } else {
+            this.token = "ring";
+        }
+    }
+}
