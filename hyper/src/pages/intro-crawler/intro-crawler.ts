7ce9bfd455486c5679e8955d71810e58c3b1a718
Andreas Wänqvist <andrkarl@gmail.com>
2023-03-24 22:01:11 +0100

    chore: cleanup

--- hyper/src/pages/intro-crawler/intro-crawler.ts
+++ hyper/src/pages/intro-crawler/intro-crawler.ts
@@ -1,21 +1,24 @@
-import { bindable } from "aurelia";
 import { DataService, IDataService } from "../../services/dataService";
 
 export class IntroCrawler {
     public film;
+    private stars: Array<HTMLDivElement> = [];
 
-    constructor(@IDataService private readonly dataService: DataService) {
-
-    }
+    constructor(@IDataService private readonly dataService: DataService) { }
 
     public async loading(parameters) {
         this.film = await this.dataService.getFromUrl(parameters[0] as string);
         this.setupBackground();
     }
 
-    setupBackground() {
+    public unloading() {
+        // remove all the stars
+        this.stars.forEach(div => div.remove());
+    }
+
+    private setupBackground(): void {
         // Sets the number of stars we wish to display
-        const numStars = 100;
+        const numStars = 500;
 
         // For every star we want to display
         for (let i = 0; i < numStars; i++) {
@@ -25,13 +28,12 @@ export class IntroCrawler {
             star.style.top = xy[0] + 'px';
             star.style.left = xy[1] + 'px';
             document.body.append(star);
-
-            //TODO: Add to array and dismantle on navigation
+            this.stars.push(star);
         }
     }
 
     // Gets random x, y values based on the size of the container
-    private getRandomPosition() {
+    private getRandomPosition(): [number, number] {
         const y = window.innerWidth;
         const x = window.innerHeight;
         const randomX = Math.floor(Math.random() * x);
