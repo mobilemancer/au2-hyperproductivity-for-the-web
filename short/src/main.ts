3263245fe470f923d78a8b564808ee858c495156
Andreas Wänqvist <andrkarl@gmail.com>
2023-03-27 20:36:28 +0200

    skeleton

--- short/src/main.ts
+++ short/src/main.ts
@@ -1,6 +1,8 @@
+import { RouterConfiguration } from '@aurelia/router';
 import Aurelia from 'aurelia';
 import { MyApp } from './my-app';
 
 Aurelia
+  .register(RouterConfiguration.customize({ useUrlFragmentHash: false }))
   .app(MyApp)
   .start();
--- short/src/my-app.ts
+++ short/src/my-app.ts
@@ -1,3 +1,2 @@
 export class MyApp {
-  public message = 'Hello World!';
 }
