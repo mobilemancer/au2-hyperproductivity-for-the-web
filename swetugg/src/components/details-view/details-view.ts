28a7ef4dce779e97812171bd2acbc986a738de3c
Andreas Wänqvist <andrkarl@gmail.com>
2023-10-20 00:00:00 +0200

    spinners

--- swetugg/src/components/details-view/details-view.ts
+++ swetugg/src/components/details-view/details-view.ts
@@ -1,13 +1,17 @@
 import { ISwapiService, SwapiService } from "../../services/swapiService";
 
 export class DetailsView {
+    public fetchingdata = false;
     public entity: object = undefined;
 
     constructor(@ISwapiService private readonly swapiService: SwapiService) { }
 
     async loading(params: object) {
         if (params[0]) {
+            this.entity = undefined;
+            this.fetchingdata = true;
             this.entity = await this.swapiService.getByUrl(params[0]);
+            this.fetchingdata = false;
         }
     }
 
